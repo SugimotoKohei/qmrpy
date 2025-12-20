94029bc095901404898e0b3fca6327fe9b4a9dc8
Sugimoto Kohei <sugimotokohei@gmail.com>
2025-12-20 16:15:37 +0900

    ✨ feat(qsm): add split-bregman and L2 reconstruction

--- src/qmrpy/models/__init__.py
+++ src/qmrpy/models/__init__.py
@@ -1,15 +1,18 @@
 from .b1 import B1Dam
 from .noise import MPPCA
+from .qsm import qsm_split_bregman, calc_chi_l2
 from .t1 import InversionRecovery, VfaT1
 from .t2 import DecaesT2Map, DecaesT2Part, MonoT2, MultiComponentT2
 
 __all__ = [
     "B1Dam",
+    "calc_chi_l2",
     "DecaesT2Map",
     "DecaesT2Part",
     "InversionRecovery",
     "MonoT2",
     "MultiComponentT2",
     "MPPCA",
+    "qsm_split_bregman",
     "VfaT1",
 ]
---
+++ src/qmrpy/models/qsm/__init__.py
@@ -0,0 +1,3 @@
+from .split_bregman import qsm_split_bregman, calc_chi_l2
+
+__all__ = ["qsm_split_bregman", "calc_chi_l2"]
